aecc4ef9bfbf2128c2aa2e00f3e2a1d234432b21
Elijah Wendel <ezwendel@gmail.com>
2021-04-16 21:36:56 -0400

    routing/frontend start

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,14 @@
 
+const testRoutes = require('./books')
+const userRoutes = require('./reviews')
+
+const constructorMethod = (app) => {
+    app.use('/tests', testRoutes)
+    app.use('/users', userRoutes)
+
+    app.use('*', (req, res) => {
+        res.status(404).json({ error: 'Route not found' })
+    })
+}
+
+module.exports = constructorMethod;
--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,14 @@
 
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
+
+router.get('/:id', async (req, res) => {
+    
+})
+
+router.post('/', async (req, res) => {
+    
+})
+
+module.exports = router;
